942d9b08b1952b4722a5c08186d1fcf5ade1acd6
juanfe428 <juanfelipesandoval2000@gmail.com>
2018-05-30 17:50:34 -0500

    modificar posicion y obtener matriz completa agregado y funcional

--- ListasFilas.cpp
+++ ListasFilas.cpp
@@ -13,6 +13,8 @@ void MatrizDispersa::imprimir(){
 }
 void MatrizDispersa::crearDeMatrizCompleta(vector< vector<int> >& matrix){
     Lista lst;
+    m = matrix.size();
+    n = matrix[0].size();
     for (int i = 0; i < matrix.size(); i++){
         valores.push_back(lst);
     }
@@ -22,4 +24,39 @@ void MatrizDispersa::crearDeMatrizCompleta(vector< vector<int> >& matrix){
                 valores[fil].anxLista(matrix[fil][col], col);
         }
     }
+}
+vector<vector<int>> MatrizDispersa::obtenerMatrizCompleta(){
+    vector<vector<int>> matriz_completa(m, vector<int>(n));
+    for(int i = 0; i < m ; i++ ){
+        for(int j = 0 ; j < n ; j++){
+            matriz_completa[i][j] = 0;
+        }
+    }
+    for (int fil = 0; fil < valores.size(); fil++){
+        for (int col = 1; col <= valores[fil].longLista(); col++){
+            Nodo *n = valores[fil].getNodo(col);
+            matriz_completa[fil][valores[fil].infoCol(n)] = valores[fil].infoVal(n);
+        }
+    }
+    return matriz_completa;
+}
+
+int MatrizDispersa::obtenerElemento(int i , int j){
+    if(valores[i].vaciaLista()){
+        return 0;
+    }
+    for(int n = 0 ; n <= valores[i].longLista(); n++){
+        Nodo *t = valores[i].getNodo(n);
+        if(valores[i].infoCol(t) == j ){
+            return valores[i].infoVal(t);
+        }
+    }
+    return 0;
+}
+vector<int> MatrizDispersa::obtenerFila(int i){
+    vector<int> fila;
+    for(int n = 0 ; n <= valores[i].longLista(); n++){
+        fila.push_back(valores[i].infoCol(valores[i].getNodo(n)));
+    }
+    return fila;
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "formatocoordenado.h"
+#include "ListasFilas.h"
+#include "lista.h"
 #include <vector>
 
 int main(){
@@ -15,21 +17,30 @@ int main(){
             cin >> matrix[fil][col];
         }
     }
-    FormatoCoordenado f1;
+    MatrizDispersa f1;
     
 
     f1.crearDeMatrizCompleta(matrix);
     cout << "creada: "<<endl;
     f1.imprimir();
     matrix2 = f1.obtenerMatrizCompleta();
+    /*
     v = f1.obtenerFila(1);
     f1.modificarPosicion(0,1,994);
     matrix2 = f1.obtenerMatrizCompleta();
     cout << "modificada: "<<endl;
     f1.imprimir();
-
     cout << "===============" <<endl;
 
+    for (int fil = 0; fil < matrix2.size(); fil++)
+    {
+        for (int col = 0; col < matrix2[fil].size(); col++)
+        {
+            cout << "Matrix[" << fil << "][" << col << "]\n";
+            cout << matrix2[fil][col] << endl;
+        }
+    }*/
+    
     for (int fil = 0; fil < matrix2.size(); fil++)
     {
         for (int col = 0; col < matrix2[fil].size(); col++)
